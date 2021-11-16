83329e21443708c8947d1c100b00ff8407162b70
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2021-11-16 18:49:52 +0530

    adminapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 
-function App() {
+
+export default function App() {
+  
+  const history = useHistory();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+       <Toolbar>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/")}>Home</Button>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/dashboard")}>Dashboard</Button>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/button")}>Button</Button>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/chart")}>Chart</Button>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/page")}>404 page</Button>
+       <Button varient="text" color="inherit" onClick={()=>history.push("/blankpage")}>Blank page</Button>
+       </Toolbar>
+       </AppBar>
+
+       <Switch>
+      
+      <Route exact path="/">
+      <Home />
+        </Route>
+        
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+
+        <Route path="/button">
+        <Abutton/>
+        </Route>
+
+        <Route path="/chart">
+          <Chart />
+        </Route>
+
+        <Route path="/page">
+          <Page />
+        </Route>
+
+        <Route path="/blankpage">
+          <Blank />
+        </Route>
+
+        </Switch>
+    </div>
+  );
+}
+function Dashboard() {
+  return (
+    <div className="home">
+      <h2 className="home-hello">Dashboard</h2>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="home">
+      <h2 className="home-hello">Home</h2>
+    </div>
+  );
+}
+
+function Abutton() {
+  return (
+    <div className="color-button" >
+     <h1>textcolor</h1>
+    </div>
+  );
+}
+
+function Chart() {
+  return (
+    <div className="home">
+      <h2 className="home-hello">Chart</h2>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <div className="not-found">
+      <img src="https://cdn.dribbble.com/users/381530/screenshots/3949858/404.gif" alt="404 not-found"/>
+    </div>
+  );
+}
+
+function Blank() {
+  return (
+    <div>
+      <h2 className="blank-page">Blank page</h2>
+    </div>
+  );
+}
